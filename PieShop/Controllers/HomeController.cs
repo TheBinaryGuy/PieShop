4420e2413237241f92f9de8927603f6b547e79bd
TheBinaryGuy <heet_mehta@hotmail.com>
2019-01-13 22:20:45 +0530

    Initial Commit

---
+++ PieShop/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PieShop.Models;
+using PieShop.ViewModels;
+using System.Linq;
+
+namespace PieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController(IPieRepository pieRepository)
+        {
+            PieRepository = pieRepository;
+        }
+
+        private IPieRepository PieRepository { get; }
+
+        public IActionResult Index()
+        {
+            var pies = PieRepository.GetAllPies().OrderBy(p => p.Name);
+            var model = new HomeViewModel
+            {
+                Title = "Pie Shop",
+                Pies = pies.ToList()
+            };
+            return View(model);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var pie = PieRepository.GetPieById(id);
+            if (pie == null) return NotFound();
+            return View(pie);
+        }
+    }
+}
